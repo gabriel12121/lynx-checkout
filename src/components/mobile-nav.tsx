d1dbe2a534eb328ed869f775b4cdb43821e3af15
gabriel12121 <contatogabrielsouza21@gmail.com>
2024-07-12 17:56:07 -0300

    mobile

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -102,7 +102,7 @@ export default function  MobileNav(){
                               <Link
                 href="/settings/logistica"
                 className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                prefetch={false}
+                
               >
                 Logistica
               </Link>
