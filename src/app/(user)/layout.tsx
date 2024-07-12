c26d04fa2f48f85e6bc6344082fc244b4aa39da7
gabriel12121 <contatogabrielsouza21@gmail.com>
2024-07-12 14:25:43 -0300

    modific page

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -3,16 +3,20 @@ import Navbar from '@/components/navbar'
 import Header from '@/components/header'
 import PageWrapper from '@/components/pagewrapper'
 
-export default function User({ children }: { children: React.ReactNode }) {
+export default function User({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
     return (
         <>
         <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
             <Navbar />
           <div className="flex flex-col">
              <Header />
-            <PageWrapper children={children} />
+            <PageWrapper>{children}</PageWrapper>
           </div>
         </div>
         </>
-    )
+    );
 }
